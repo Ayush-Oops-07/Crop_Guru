c7ff1d4b630491f346f3648098cfd5d3130e1a23
Ayush Kumar Sharma <starksystem07@gmail.com>
2025-09-13 16:22:30 +0530

    Update page.tsx

--- web/app/weather/page.tsx
+++ web/app/weather/page.tsx
@@ -13,7 +13,7 @@ export default function WeatherPage() {
     setLoading(true);
     setError("");
     try {
-      const res = await fetch(`http://127.0.0.1:8000/weather?city=${city}`);
+      const res = await fetch(`http://crop-guru.onrender.com/weather?city=${city}`);
       const data = await res.json();
 
       if (data.error) {
