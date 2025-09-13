e495b317ab017822bb951247e7bf9e1e25fa3a88
Ayush Kumar Sharma <starksystem07@gmail.com>
2025-09-13 16:22:51 +0530

    Update page.tsx

--- web/app/soil/page.tsx
+++ web/app/soil/page.tsx
@@ -13,7 +13,7 @@ export default function SoilPage() {
     setLoading(true);
     setError("");
     try {
-      const res = await fetch(`http://127.0.0.1:8000/soil/${city}`);
+      const res = await fetch(`http://crop-guru.onrender.com/soil/${city}`);
       const data = await res.json();
 
       if (data.error) {
